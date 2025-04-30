9e9d465e53a824b41be35fe449571c7e228740f2
PEDRO MARTINEZ HUERTAS <145330756+MrCordobex@users.noreply.github.com>
2025-04-30 12:54:31 +0200

    Update inference.py

--- utils/inference.py
+++ utils/inference.py
@@ -72,9 +72,9 @@ def yolo_prediction(df_cleaned,model_path,input,output,filter,conf_threshold=0.2
 
     # 3) Definir pesos (deben sumar 1)
     weights = {
-        'n_dets_norm':     0.3,
+        'n_dets_norm':     0.2,
         'n_classes_norm':  0.2,
-        'score_max_norm':  0.4,
+        'score_max_norm':  0.5,
         'area_total_norm': 0.1
     }
 
